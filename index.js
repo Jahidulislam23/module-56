5b3cb749cc08cf8073496e4dfade89fdbb310e51
Jahidulislam23 <jahidul110j@gmail.com>
2025-05-14 11:41:36 +0600

    added the addcoffee

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require( 'express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgx1gaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,16 +18,32 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeesCollection = client.db("coffeeDB").collection("coffees");
+
+    // app.post("/coffees", async (req, res) => {
+    //   const newCoffee = req.body;
+    //   console.log(newCoffee);
+    //   const result = await coffeesCollection.insertOne(newCoffee);
+    //   res.send(result);
+    // });
+    app.get('/coffees',async(req,res)=>{
+        const result = await coffeesCollection.find().toArray()
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -35,11 +51,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/',(req,res)=>{
-    res.send('coffee server is getting hotter ')
+app.get("/", (req, res) => {
+  res.send("coffee server is getting hotter ");
 });
 
-app.listen(port,()=>{
-    console.log('coffee server is running on port',port)
-})
+app.listen(port, () => {
+  console.log("coffee server is running on port", port);
+});
